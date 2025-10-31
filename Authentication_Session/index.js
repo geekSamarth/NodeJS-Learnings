be976380d2a2d8b19e63ca287d74a0f7fad83037
geeksamarth <samarthagrawal1804@gmail.com>
2025-10-31 15:44:46 +0530

    bug fixes: do some minor changes in the middleware export

--- Authentication_Session/index.js
+++ Authentication_Session/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
 import adminRouter from "./routes/admin.routes.js";
-import { authenticatedMiddlware } from "./middlewares/auth.middlewares.js";
+import { authenticationMiddleware } from "./middlewares/auth.middlewares.js";
 const app = express();
 const PORT = process.env.PORT ?? 8000;
 app.use(express.json());
-app.use(authenticatedMiddlware);
+app.use(authenticationMiddleware);
 app.get("/", (req, res) => {
   return res.json({ status: "Server is up and running." });
 });
